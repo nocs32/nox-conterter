d21ce50417dc7ff5fbaf18f2ec80fed092d28f83
Tymofii Tytenko <tank521611@gmail.com>
2024-09-10 15:53:37 +0300

    add github action, update favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
     title: "NOX CONVERTER",
-    description: "NOX CONVERTER lets you change the file formats of your images",
+    description: "NOX CONVERTER lets you change the file formats of your files",
 };
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
